65b4309ca2e2db1280228ee7a6824a6d9e1b7e77
lovezsh <shuxhan@163.com>
2023-02-20 17:46:55 +0800

    Add files via upload

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,23 @@
 chrome.bookmarks.getTree(function (bookmarkTreeNodes) {
   // 处理书签树节点数据
   const data = bookmarkTreeNodes[0].children[0].children;
-  // console.log(data);
-
+  console.log(data);
+  localStorage.setItem("bookmarksData", JSON.stringify(data));
 
   $.each(data, function (i) {
     var $li = $("<li class='" + i + "'>" + ($(this)[0].title) + "</li>")
     $("#ul").append($li);
-
     const children = $(this)[0].children;
 
+
+    
+
     $.each(children, function () {
-      var $item = $("<li class='" + i + "'><a href=' " + ($(this)[0].url) + "'>" + ($(this)[0].title) + "</a></li>")
+      var $item = $("<li class='" + i + "'><a href=' " + ($(this)[0].url) + "' target='_blank'>" + ($(this)[0].title) + "</a></li>")
       $('#content').append($item);
     })
 
+    $('#content li').hide();
   })
 
   $('#ul li').each(function (i) {
@@ -22,12 +25,11 @@ chrome.bookmarks.getTree(function (bookmarkTreeNodes) {
       const liclass = $(this)[0].className;
       // console.log(liclass);
       // console.log(i);
+
       $('#ul li').removeClass('active')
       $(this).addClass('active')
-
-      $('#content li:nth-child(5)').animate({
-        scrollTop: 100
-      }, 800);
+      $('#content li').hide();
+      $('#content li.' + liclass + '').show();
     })
   })
 
